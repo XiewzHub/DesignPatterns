a956df9d91f712f7eae1e3aa34997a019983fbf5
XiewzHub <673275656@qq.com>
2019-07-12 21:59:51 +0800

    修改

--- Decorator/src/mode/ConcreteComponent.java
+++ Decorator/src/mode/ConcreteComponent.java
@@ -4,6 +4,6 @@ public class ConcreteComponent extends Component{
 
     @Override
     public void operation() {
-        System.out.println("具体对象的操作，组件");
+        System.out.println("具体对象的操作");
     }
 }
